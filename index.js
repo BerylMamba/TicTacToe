7b6f936ef54f9a3c13802fa2f159fb6c9a68ad72
Bilal Mirza <mirzaaghabilal123@gmail.com>
2022-08-28 14:47:44 +0100

    no more reocccuring board and X now appears when box is clicked

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const state= {
     gameElement: document.querySelector('.game'),
-    cells: Array(9).fill(null)
+    cells: Array(9).fill(null),
+    symbols: ['o', 'X']
 }
 
 
 function drawboard() {
+
+    console.log("drawBoard has run")
+    state.gameElement.innerHTML = ''
 
     for (let i=0; i < 9; i++) {   
         const cell = document.createElement('div')
@@ -12,22 +16,27 @@ function drawboard() {
 
         if (state.cells[i]) {  // does the cell have an X or an O? if so, this code runs
 
-            const cellSymbol = document.createElement('p') // <p></p> 
-            cellSymbol.innerText = 'Jeff'
-            cell.append
+            const cellSymbol = document.createElement('p') // <p class="symbol"></p> 
+            cellSymbol.innerText = 'X'
+            cellSymbol.classList.add('symbol')
 
-
+            cell.append(cellSymbol)
         } else { // otherwise it will detect the cell is empty and will run this next
             cell.addEventListener('click', function (){
                 state.cells[i] = 'x'
 
-                drawboard()
-        })
+                // console.log('cell ${i} was clicked')
+                // console.log('i added an x to the celles array at index')
+                // console.log(state.cells)
 
+
+
+                drawboard()
+            })
+        }
 
         state.gameElement.append(cell)
 
-        }
     }
 }
 drawboard()
