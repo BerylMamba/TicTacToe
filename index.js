74e89791303e7dec67fd817438220d2c7d8e9342
Bilal Mirza <mirzaaghabilal123@gmail.com>
2022-08-28 15:47:53 +0100

    added x and o to alternate for every move

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+/* Creating a variable called state and assigning it to an object. The object has three properties:
+gameElement, cells, and symbols. */
 const state= {
     gameElement: document.querySelector('.game'),
     cells: Array(9).fill(null),
-    symbols: ['o', 'X']
+    symbols: ['o', 'x']
 }
 
 
 function drawboard() {
 
     console.log("drawBoard has run")
-    state.gameElement.innerHTML = ''
+    state.gameElement.innerHTML = '' /* Clearing the board so it does not repeat itself */
+    
+    /* Creating a div with the class of cell. */
 
     for (let i=0; i < 9; i++) {   
         const cell = document.createElement('div')
@@ -17,19 +21,19 @@ function drawboard() {
         if (state.cells[i]) {  // does the cell have an X or an O? if so, this code runs
 
             const cellSymbol = document.createElement('p') // <p class="symbol"></p> 
-            cellSymbol.innerText = 'X'
+            cellSymbol.innerText = state.cells[i]
             cellSymbol.classList.add('symbol')
 
             cell.append(cellSymbol)
         } else { // otherwise it will detect the cell is empty and will run this next
             cell.addEventListener('click', function (){
                 state.cells[i] = 'x'
+                state.symbols.reverse()
+                state.cells[i] = state.symbols[0]
 
                 // console.log('cell ${i} was clicked')
                 // console.log('i added an x to the celles array at index')
-                // console.log(state.cells)
-
-
+                console.log(state.cells)
 
                 drawboard()
             })
